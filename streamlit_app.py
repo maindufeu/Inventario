c2e568b0b83d3c8ef54f252829c875346a4df83e
Edher Jiménez <edher.jimenez@gmail.com>
2023-06-20 14:20:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,21 +11,18 @@ import numpy as np
 class OCR:
 
     def __init__(self):
-        #altera titulo da pagina
-        #st.set_page_config(page_title="Sistema de inventario OCR")
-        #inicializa variveis
         self.texto = ""
-        self.analisar_texto = False
+        self.analizar_texto = False
 
     def inicial(self):
-        #conteudo inicial da pagina
-        st.title("Sistema integrado JMP")
+        #Contenido inicial de la página
+        st.title("Sistema integrado de inventarios")
         option = st.selectbox('Proveedor', ('Proveedor 1', 'Proveedor 2', 'Proveedor 3', 'Otro'))
         st.write('Proveedor:', option)
         num_prod = st.number_input('Número de productos', min_value=1, max_value=1000000, value=1, step=1)
         st.write("Escaneo de etiqueta")
         imagem = st.file_uploader("escanear etiqueta", type=["png","jpg"])
-        #se selecionar alguma imagem...
+        #Se selecciona alguna imagen...
         if imagem:
             img = Image.open(imagem)
             st.image(img, width=350)
@@ -51,7 +48,28 @@ class OCR:
             #self.analisar_texto = st.sidebar.checkbox("Analizar texto")
             #if self.analisar_texto==True:
             #    self.mostrar_analise()
-    
+        
+        picture = st.camera_input("Take a picture")
+        if picture:
+            st.image(picture, width=350)
+            st.info("Texto extraído")
+            self.texto = self.extrair_texto(img)
+            st.write(f"{self.texto}")
+            if "SKU" in self.texto:
+              sku = self.texto.split("SKU")[1].split("\n")[0]
+              sku = [int(x) for x in sku.split() if x.isdigit()][0]
+              st.write(f"SKU del producto registrado: {sku}")
+
+            if "RFC" in self.texto:
+              rfc = self.texto.split("RFC")[1].split("\n")[0]
+              rfc = rfc.replace('#', '').replace(' ','').replace(':','')
+              st.write(f"RFC del vendedor registrado: {rfc}")
+            
+            if st.button('Enviar'):
+                st.write('Enviado')
+            else:
+                st.write('Revisa la entrada antes de enviar')
+                
     def extrair_texto(self, img):
         #O comando que extrai o texto da imagem
         texto = pytesseract.image_to_string(img)

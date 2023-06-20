35c433151d731d59439970016e44df747f77b32c
Edher Jiménez <edher.jimenez@gmail.com>
2023-06-20 14:01:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ class OCR:
 
     def __init__(self):
         #altera titulo da pagina
-        #st.set_page_config(page_title="Sistema integrado JMP")
+        #st.set_page_config(page_title="Sistema de inventario OCR")
         #inicializa variveis
         self.texto = ""
         self.analisar_texto = False
@@ -20,7 +20,7 @@ class OCR:
     def inicial(self):
         #conteudo inicial da pagina
         st.title("Sistema integrado JMP")
-        option = st.selectbox('Proveedor', ('Chino 1', 'Chino 2', 'Chino 3', 'Otro'))
+        option = st.selectbox('Proveedor', ('Proveedor 1', 'Proveedor 2', 'Proveedor 3', 'Otro'))
         st.write('Proveedor:', option)
         num_prod = st.number_input('Número de productos', min_value=1, max_value=1000000, value=1, step=1)
         st.write("Escaneo de etiqueta")
@@ -57,38 +57,6 @@ class OCR:
         texto = pytesseract.image_to_string(img)
         return texto
     
-    '''def mostrar_analise(self):
-        #busca CPF, datas e palavras boas e mas na extracao
-     #   cpf = fc.buscar_cpf(self.texto)
-      #  datas = fc.buscar_data(self.texto)
-       # p_boas, percentual_bom = fc.buscar_palavras_boas(self.texto)
-        p_mas, percentual_mau = fc.buscar_palavras_mas(self.texto)
-        
-        if cpf==None:
-            st.warning("Nenhum CPF encontrado.")
-        else:
-            cpf = fc.sumarizar_cpf(cpf)
-            st.success("CPF encontrado:")
-            st.write(cpf)
-
-        if datas==None:
-            st.warning("Nenhuma data encontrada.")
-        else:
-            datas = fc.sumarizar_datas(datas)
-            st.success("Datas encontradas:")
-            st.write(datas)
-        
-        if p_boas==0:
-            st.warning("Não identificado palavras de bem.")
-        else:
-            st.success("Palavras de bem:")
-            st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_boas, percentual_bom))
-        
-        if p_mas==0:
-            st.warning("Não identificado palavras más.")
-        else:
-            st.success("Palavras más:")
-            st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_mas, percentual_mau))'''
 
 ocr = OCR()
 ocr.inicial()
